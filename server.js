9c1e9a26bd654edf7d2690e0b0c47f6e468be334
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-03-05 19:24:38 -0600

    Defined a API Get route to retrieve notes data as JSON. Also, placed wildcard route at the bottom of the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-
-const PORT = process.env.PORT || 3001;
+const notesData = require('./db/db.json')
+const PORT = 3001;
 
 const app = express();
 
@@ -18,7 +18,13 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-    // GET wildcard route
+// API Routes
+    // API GET route to retrieve notes
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+
+
+// GET wildcard route
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
