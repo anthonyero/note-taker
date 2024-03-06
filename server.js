f45a37ba7a7dab6c905d05339d980aaeb6177b57
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-03-05 18:59:18 -0600

    Added a GET route to the homepage and a GET route to the notes.html page

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const app = express();
 
 app.use(express.static('public'));
 
+// GET Route for the homepage
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+); 
+
+// HTML Routes
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
