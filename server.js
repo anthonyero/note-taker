17958f002da62b63012faf3c1f20beae299be7e1
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-03-05 19:02:17 -0600

    Added a wildcard GET route

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.get('/', (req, res) =>
 ); 
 
 // HTML Routes
-
+    // GET route to the "Notes" page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+    // GET wildcard route
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
