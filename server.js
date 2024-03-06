0e72e2486644735402195c061e0263e7b518dee3
Anthony Rodriguez-Ortiz <anthonyero@gmail.com>
2024-03-05 20:55:59 -0600

    Defined a POST route to add a note. Also, imported the UUID and FS packages

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const UUID = require('uuid'); // Utilized to generate a note id
+const fs = require('fs'); // 
 const notesData = require('./db/db.json')
 const PORT = 3001;
 
 const app = express();
 
+// Middleware
 app.use(express.static('public'));
+app.use(express.json())
+
 
 // GET Route for the homepage
 app.get('/', (req, res) => 
@@ -21,6 +26,41 @@ app.get('/notes', (req, res) => {
 // API Routes
     // API GET route to retrieve notes
 app.get('/api/notes', (req, res) => res.json(notesData));
+
+    // API POST route to add a note to the db.json file
+app.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title, 
+            text,
+            id: UUID.v4()
+        };
+
+        // Appends newNote object to existing notesData
+        notesData.push(newNote);
+        // Convert newNote object to a string
+        let updatedDataString = JSON.parse(notesData);
+
+        fs.writeFile('./db/db.json', updatedDataString, (err) => 
+            err
+            ? console.error(err)
+            : console.log(
+                `${newNote} has been written to JSON file`
+              ) 
+        );
+        
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in adding note')
+    };
+});
 
 
 
